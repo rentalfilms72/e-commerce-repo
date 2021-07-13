a6952a7056ef0f25fcb99ccca2451ec27938e4b1
ledjizalfra <djizalfra@gmail.com>
2021-07-13 14:17:33 +0200

    UPDATE

--- config-update-repository.sh
+++ config-update-repository.sh
@@ -11,7 +11,7 @@ function pause(){
 git add .
 
 #@echo "adding url of distant repository"
-git remote add myorigin https://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@github.com/rentalfilms72/e-commerce-repo.git
+git remote add myorigin https://rentalfilms72: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@github.com/rentalfilms72/e-commerce-repo.git
 
 #@echo "Our first commit..."
 git commit -m "UPDATE"
